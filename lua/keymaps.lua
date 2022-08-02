a645124b52547eb59cfc4d6833b47ccf89427364
Ying Fang <fangying712@gmail.com>
2022-08-02 10:27:56 +0000

    keymaps.lua: Support moving buffer up and down in visual mode
    Signed-off-by: Ying Fang <fangying712@gmail.com>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,3 +6,6 @@ vim.keymap.set("n", "<leader>gg", "<Cmd>Neogit<CR>", { desc = "Open Neogit" })
 vim.keymap.set("n", "<leader>gdo", "<Cmd>DiffviewOpen<CR>", { desc = "Open" })
 vim.keymap.set("n", "<leader>gdc", "<Cmd>DiffviewClose<CR>", { desc = "Close" })
 vim.keymap.set("n", "<leader>gdh", "<Cmd>DiffviewFileHistory<CR>", { desc = "Open History" })
+-- Move bufferline selected up and down in visual mode
+vim.keymap.set("v", "J", ":move '>+1<CR>gv-gv", { desc = "Move Down" })
+vim.keymap.set("v", "K", ":move '<-2<CR>gv-gv", { desc = "Move Up" })
